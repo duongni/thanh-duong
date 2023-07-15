ce08c5f989d7418b86c5468de231c162624e9930
duongni <133175915+duongni@users.noreply.github.com>
2023-07-14 23:17:41 -0400

    created a base structure for digital business card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import thanhheadshot2 from "./thanhheadshot2.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="profile">
+        <div className="headshot">
+          <img
+            src={thanhheadshot2}
+            alt="Thanh Duong's Profile"
+            className="headshot"
+          />
+        </div>
+        <h1>Thanh Duong</h1>
+        <p>Financial Solution</p>
+        <button type="button" class="btn btn-primary">
+          Learn More About Thanh
+        </button>
+        <div className="who-we-are">
+          <h5>Why Choose Experior Financial Group?</h5>
+          <iframe
+            className="video"
+            src="https://www.youtube.com/embed/Rwr-_3s_0es"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
+
+        <button type="button" class="btn btn-primary contact">
+          Exchange Contact
+        </button>
+      </div>
     </div>
   );
 }
